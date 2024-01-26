7b48eee5af5eeab286cf4239338307fe9f20ca1b
Zywl <86253343+opZywl@users.noreply.github.com>
2024-01-26 19:05:55 -0300

    introdução

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,3 +1,13 @@
+/*
+ * Este arquivo faz parte do projeto PetModel (https://github.com/opZywl/PetModel-Frontend)
+ * 
+ * Copyright (c) 2024 - 2024 opZywl
+ * 
+ * Certifique-se de estar com o servidor backend em execução antes de iniciar o frontend para garantir
+ * que as chamadas à API possam ser processadas corretamente. (https://github.com/opZywl/PetModel-Backend)
+ *
+ * Este projeto foi desenvolvido como parte do Desafio da NectarLabs para o controle de adoção de pets por uma ONG.
+ */
 import React, { useState } from 'react';
 import { login } from '../api';
 
